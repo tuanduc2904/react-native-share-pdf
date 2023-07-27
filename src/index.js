219f1dab7be7712d8d98b997b9908f9374a0bcee
Duc Nguyen <tuanduc2904@gmail.com>
2023-07-27 14:05:35 +0700

    share file modules

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const { RNShareFile } = NativeModules;
 export default {
   sharePDF: async (base64Data: string, provider: string, documentFileName: string): Promise<Error | void> => {
     try {
+      
       RNShareFile.share(base64Data, provider, documentFileName);
     } catch (err) {
       return err;
